09f5ec7731a8b9bc4a596f10a0c24a106adfcffb
ngh_0924 <ngh0924@gmail.com>
2021-03-28 18:00:09 +0900

    03/28 moving to Kotlin-2

---
+++ app/src/main/java/com/example/today_seyebrowktver/CustomersData.kt
@@ -0,0 +1,7 @@
+package com.example.today_seyebrowktver
+
+data class CustomersData(
+    var customersName: String? = null,
+    var customersNumber: String? = null,
+    var customersHistory: String? = null
+)
