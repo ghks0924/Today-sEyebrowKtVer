0f4325fc172640ce42521e9ff9b2b33c7fcf7a6d
ngh_0924 <ngh0924@gmail.com>
2021-03-29 15:44:50 +0900

    03/28 moving to Kotlin-3

---
+++ app/src/main/java/com/example/today_seyebrowktver/BottomSheetFragmentCustomer.kt
@@ -0,0 +1,45 @@
+package com.example.today_seyebrowktver
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
+
+
+class BottomSheetFragmentCustomer : BottomSheetDialogFragment() {
+
+    private val mContext: Context? = context
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.bottomsheet_addcus, container, false)
+        val manualCreateBtn: MaterialButton = view.findViewById(R.id.manuallyAdd)
+        val fromBookBtn: MaterialButton = view.findViewById(R.id.fromPhonebook)
+
+        manualCreateBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(context, CreateCustomerActivity::class.java)
+            startActivity(intent)
+            dismiss()
+        })
+
+        fromBookBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(context, CreateCustomerActivity::class.java)
+            startActivity(intent)
+            dismiss()
+        })
+
+        return view
+    }
+
+
+}
