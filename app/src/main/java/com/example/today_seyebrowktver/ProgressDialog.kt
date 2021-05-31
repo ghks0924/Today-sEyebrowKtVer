f7ff283ad63a159a37df6afa2e31db8037ef05bf
ngh_0924 <ngh0924@gmail.com>
2021-05-31 22:17:57 +0900

    05/31 at StudyCafe

--- app/src/main/java/com/example/today_seyebrowktver/ProgressDialog.kt
+++ app/src/main/java/com/example/today_seyebrowktver/ProgressDialog.kt
@@ -11,7 +11,7 @@ class ProgressDialog(context : Context) {
     fun start(context: Context){
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(R.layout.dialog_progressbar)     //다이얼로그에 사용할 xml 파일을 불러옴
-        dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
         dlg.show()
     }
