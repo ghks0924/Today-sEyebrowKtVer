1e7ffc169efd037e535731ff3062db4383e82b06
ngh_0924 <ngh0924@gmail.com>
2021-08-20 23:22:21 +0900

    08/20 at 스타벅스 with 그램

--- app/src/main/java/com/example/today_seyebrowktver/ui/ActivityMonthlyCustomerList.kt
+++ app/src/main/java/com/example/today_seyebrowktver/ui/ActivityMonthlyCustomerList.kt
@@ -3,10 +3,30 @@ package com.example.today_seyebrowktver.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.today_seyebrowktver.R
+import com.example.today_seyebrowktver.databinding.ActivityMonthlyCustomerListBinding
+import com.google.android.material.tabs.TabLayout
 
 class ActivityMonthlyCustomerList : AppCompatActivity() {
+
+    //viewbinding
+    private lateinit var binding : ActivityMonthlyCustomerListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_monthly_customer_list)
+        binding = ActivityMonthlyCustomerListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
